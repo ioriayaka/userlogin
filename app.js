b75e037c939b2071099de512b873564999b73e92
ioriayaka <ioriayaka@hotmail.com>
2021-07-15 09:50:45 +0800

    add mongooses function

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
-
+require('./config/mongoose')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
